f6169b5cb1248df81b93ccbf7d3366b6fab5edaf
Raghavsharma1308 <raghavattri13@gmail.com>
2025-07-02 18:25:52 +0530

    Removed Unncessary code

--- src/main/java/com/Beginner/RestApi/RestApiApplication.java
+++ src/main/java/com/Beginner/RestApi/RestApiApplication.java
@@ -3,13 +3,11 @@ package com.Beginner.RestApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
 
 @SpringBootApplication
 public class RestApiApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Spring application loaded");
 		SpringApplication.run(RestApiApplication.class, args);
 	}
 
